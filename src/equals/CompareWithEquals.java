f9be15cdbb0256995cc601da670cad5f71735d49
ATEIS\a21diegobc <boullo88@gmail.com>
2022-12-05 17:47:54 +0100

    corrección menor: escape de comilla

--- src/equals/CompareWithEquals.java
+++ src/equals/CompareWithEquals.java
@@ -1,2 +1,29 @@
-package equals;public class CompareWithEquals {
+class Persona {
+    private final String nombre;
+package com.empresa.tutorial_java.equals;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
 }
+
+public class CompareWithEquals {
+    class CompareWithEquals {
+
+        public static void main(String... argv) {
+
+            @@ -23,7 +17,7 @@ public static void main(String... argv) {
+                String s1 = "hola";
+                String s2 = s1;
+                String s3 = "hola";
+                String s4 =  new String("hola"); // aunque el IDE sugiere no hacer explicita la instancia,...
+                String s4 = new String("hola"); // aunque el IDE sugiere no hacer explicita la instancia,...
+
+                System.out.println(s1 == s2); // true
+                System.out.println(s1 == s3); // true
+                System.out.println(s1 == s4); // false: referencias distintas
+                System.out.println(s1.equals(s2)); // true
+                System.out.println(s1.equals(s3));  // true
+                System.out.println(s1.equals(s4)); // true: referencias distintas a objetos iguales
+            }
+        }
