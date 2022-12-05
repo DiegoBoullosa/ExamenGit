d7ed70cb081c93b78992f8ffa8167740caba49f4
ATEIS\a21diegobc <boullo88@gmail.com>
2022-12-05 17:35:35 +0100

    merge de comparando_strings solventando conflicto

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-class Persona {
-    private final String nombre;
-
+@@ -4,12 +4,12 @@ class Persona {
     public Persona(String nombre) {
         this.nombre = nombre;
     }
@@ -10,13 +8,29 @@ class Persona {
 public class CompareWithEquals {
 
     public static void main(String... argv) {
+
         Persona p = new Persona("Pepe");
         Persona p1 = new Persona("Pepe");
         Persona p2 = p;
+        @@ -18,5 +18,20 @@ public static void main(String... argv) {
+            System.out.println(p.equals(p1)); // false
+            System.out.println(p == p2); // true
+            System.out.println(p.equals(p2)); // true
 
-        System.out.println(p == p1); // false
-        System.out.println(p.equals(p1)); // false
-        System.out.println(p == p2); // true
-        System.out.println(p.equals(p2)); // true
+
+            String s1 = "hola";
+            String s2 = s1;
+            String s3 = "hola";
+            String s4 =  new String("hola"); // aunque el IDE sugiere no hacer explicita la instancia,...
+
+            System.out.println(s1 == s2); // true
+            System.out.println(s1 == s3); // true
+            System.out.println(s1 == s4); // false: referencias distintas
+            System.out.println(s1.equals(s2)); // true
+            System.out.println(s1.equals(s3));  // true
+            System.out.println(s1.equals(s4)); // true: referencias distintas a objetos iguales
+
+        }
     }
+
 }
