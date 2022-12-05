f68ad183371c1db87296c2c02cbe5fa5fe2b03e9
ATEIS\a21diegobc <boullo88@gmail.com>
2022-12-05 17:53:53 +0100

    Merge branch 'valor_referencia'

---
+++ src/paso_valor/paso_objetos/Clase.java
@@ -0,0 +1,6 @@
+@Override
+public String toString() {
+        return "Objeto { i=" + entero + ", a='" + string + "\'}";
+        return "Objeto { i=" + entero + ", a='" + string + "'}";
+        }
+        }
