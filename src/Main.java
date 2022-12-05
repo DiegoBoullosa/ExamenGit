ab621a5c0e3785436a07bf3898cf59ad795cd56b
ATEIS\a21diegobc <boullo88@gmail.com>
2022-12-05 17:29:12 +0100

    ejemplos de uso de equals con objetos

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,22 @@
-import java.util.Arrays;
+class Persona {
+    private final String nombre;
 
-public class Main {
-
-    public static void main(String[] args) {
-        @@ -22,8 +24,39 @@ public static void main(String[] args) {
-            modificaContenidoObjeto(objeto2); // pasamos la referencia al objeto
-            System.out.println("Valor del objeto fuera del método: " + objeto2);
-
-
-
-            System.out.println("\n--- Pasando arrays");
-
-            int[] array1 = new int[5];
-            printArray(array1);
-            array1[0] = 0;  // innecesario ya que inicializa a 0 por defecto
-
-            int[] array2 = array1;  // dos variables apuntando al mismo array
-            int[] array3 = Arrays.copyOf(array1, array1.length); // dos arrays iguales
-
-            IncrementaArray(array1);
-            IncrementaArray(array2);
-            IncrementaArray(array3);
-
-            printArray(array1);
-            printArray(array2);
-            printArray(array3);
-        }
-
-        private static void IncrementaArray(int[] array) {
-            for (int i = 0; i < array.length; i++) {
-                array[i] = array[i] + 1;
-            }
-        }
-
-
-        private static void printArray(int[] array) {
-            Arrays.stream(array).forEach(System.out::print);
-            System.out.println();
-        }
-
-
-        private static void modificaVarInt(int a) {
-            a++; // Incrementamos en 1
-            System.out.println("Valor del int dentro del método:   " + a);
-        }
-        private static void modificaVarString(String a) {
-            a += " modificado";
-            System.out.println("Valor del string dentro del método: " + a);
-        }
-        private static void modificaVarObjeto(Clase o) {
-            o = new Clase(o.entero + 1, "nuevo string"); // creamos un nueva instancia
-            System.out.println("Valor del objeto dentro de método: " + o);
-        }
-        private static void modificaContenidoObjeto(Clase o) {
-            o.entero = -99; // modificamos el contenido de la referencia
-            o.string = "string modificado";
-            System.out.println("Valor del objeto dentro de método: " + o);
-        }
+    public Persona(String nombre) {
+        this.nombre = nombre;
     }
+
+}
+
+public class CompareWithEquals {
+
+    public static void main(String... argv) {
+        Persona p = new Persona("Pepe");
+        Persona p1 = new Persona("Pepe");
+        Persona p2 = p;
+
+        System.out.println(p == p1); // false
+        System.out.println(p.equals(p1)); // false
+        System.out.println(p == p2); // true
+        System.out.println(p.equals(p2)); // true
+    }
+}
